e2cfe41603d05f9a92ccafea48350922197db890
sashi0034 <satoshi3104.nishi@gmail.com>
2022-02-13 20:39:03 +0900

    戦に備えています

--- template.cpp
+++ template.cpp
@@ -6,6 +6,31 @@
 #include <iomanip>
 #include <limits>
 
+#define PI  3.1415926535897931;
+#define LOOP(i, max)    for (int i=0; i<max; i++){
+#define END     }
+
+using ll = long long;
+
+namespace good
+{
+    int get_order(long long n);
+    bool is_between(double n, double min, double max);
+    int get_sign(double n);
+    double get_distance(double x, double y);
+    double to_rad(double deg);
+    double to_deg(double rad);
+}
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
@@ -15,4 +40,40 @@ int main()
 
 
 
+
+
+
+
+
+
+
+
+
+namespace good
+{
+    int get_order(long long n)
+    {
+        return (int)std::log10(n) + 1;
+    }
+    bool is_between(double n, double min, double max)
+    {
+        return (min <= n && n <= max);
+    }
+    int get_sign(double n)
+    {
+        return (n > 0) - (n < 0);
+    }
+    double get_distance(double x, double y)
+    {
+        return std::sqrt(x * x + y * y);
+    }
+    double to_rad(double deg)
+    {
+        return (deg / 180.0) * PI;
+    }
+    double to_deg(double rad)
+    {
+        return (rad * 180) / PI;
+    }
+}
 
