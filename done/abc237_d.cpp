aed254cd43f5e85fe6d576e1e4be4bd8d8c77efd
sashi0034 <satoshi3104.nishi@gmail.com>
2022-02-17 12:59:38 +0900

    📒 push_backは速い件について

---
+++ done/abc237_d.cpp
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <numeric>
+
+#define PI  3.1415926535897931
+#define LL_MAX   9223372036854775807
+// LL_MAX ~= 10 ^ 18
+#define INT_MAX   2147483647
+// INT_MAX ~= 10 ^ 9
+#define LOOP(i, max)    for (int i=0; i<max; ++i){
+#define END     }
+#define INPUT(value)  std::cin >> value;
+#define OUTPUT(value)  std::cout << value << " ";
+
+using ll = long long;
+
+namespace good
+{
+    int get_order(long long n);
+    bool is_between(double n, double min, double max);
+    int get_sign(double n);
+    double get_distance(double x, double y);
+    double to_rad(double deg);
+    double to_deg(double rad);
+    std::string to_bin(long long num10);
+}
+
+
+
+
+int main()
+{
+    int N;
+    std::string S = "";
+
+    INPUT(N >> S);
+
+    auto A = std::vector<int>{0};
+    
+    auto a_l = std::vector<int>{};
+    auto a_r = std::vector<int>{};
+
+    LOOP(i, N)
+        if (S[i]=='R')
+        {
+            a_l.push_back(i);
+        }
+        else
+        {
+            a_r.push_back(i);
+        }
+        
+
+    END
+
+    std::reverse(a_r.begin(), a_r.end());
+
+    LOOP(i, a_l.size())
+        OUTPUT(a_l[i]);
+    END
+    OUTPUT(N);
+    LOOP(i, a_r.size())
+        OUTPUT(a_r[i]);
+    END
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+
+int main()
+{
+    int N;
+    std::string S = "";
+
+    INPUT(N >> S);
+
+    auto A = std::vector<int>{0};
+
+    int cur = 0;
+    for (int i=0; i<N; ++i)
+    {
+        if (S[i] == 'L')
+        {
+            A.insert(A.begin()+cur, i+1);
+        }
+        else
+        {
+            ++cur;
+            A.insert(A.begin()+cur, i+1);
+        }
+    }
+
+    LOOP(i, N+1)
+        OUTPUT(A[i]);
+    END
+
+
+    return 0;    
+}
+
+
+#endif
+
+
+
+
+
+
+
+
+
+namespace good
+{
+    int get_order(long long n)
+    {
+        return (int)std::log10(n) + 1;
+    }
+    bool is_between(double n, double min, double max)
+    {
+        return (min <= n && n <= max);
+    }
+    int get_sign(double n)
+    {
+        return (n > 0) - (n < 0);
+    }
+    double get_distance(double x, double y)
+    {
+        return std::sqrt(x * x + y * y);
+    }
+    double to_rad(double deg)
+    {
+        return (deg / 180.0) * PI;
+    }
+    double to_deg(double rad)
+    {
+        return (rad * 180) / PI;
+    }
+    std::string to_bin(long long num10)
+    {
+        std::string ret = "";
+        while(num10 != 0)
+        {
+            ret = (num10 % 2 == 0 ? "0" : "1") + ret;
+            num10 /= 2;
+        }
+        return ret;
+    }
+}
+
